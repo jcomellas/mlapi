183856a385fe11cbc443cc151d58f236f0907b2d
Juan Jose Comellas <juanjo@comellas.org>
2011-09-01 00:29:38 -0300

    Initialize cache using configuration value from environment

--- src/mlapi_app.erl
+++ src/mlapi_app.erl
@@ -1,3 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Juan Jose Comellas <juanjo@comellas.org>
+%%% @copyright (C) 2011 Juan Jose Comellas
+%%% @doc MercadoLibre API application.
+%%% @end
+%%%
+%%% This source file is subject to the New BSD License. You should have received
+%%% a copy of the New BSD license with this software. If not, it can be
+%%% retrieved from: http://www.opensource.org/licenses/bsd-license.php
+%%%-------------------------------------------------------------------
 -module(mlapi_app).
 -author('Juan Jose Comellas <juanjo@comellas.org>').
 
@@ -11,6 +21,8 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    %% Initialize the tables used for the Mnesia cache.
+    mlapi_cache:init(mlapi:get_env(cache_nodes, [node()])),
     mlapi_sup:start_link().
 
 stop(_State) ->
