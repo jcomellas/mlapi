c0a99d907f82fa30ac14df08871a0c7536fc89ab
Juan Jose Comellas <juanjo@comellas.org>
2013-05-19 14:32:22 -0300

    Make sure that the Mnesia schema has been created when started

--- src/mlapi_app.erl
+++ src/mlapi_app.erl
@@ -22,9 +22,31 @@
 
 -spec start(application:start_type(), [term()]) -> {ok, pid()}.
 start(_StartType, _StartArgs) ->
+    ok = ensure_mnesia_schema_created(node()),
     %% Initialize the tables used for the Mnesia cache.
     mlapi_cache:init(mlapi:get_env(cache_nodes, [node()])),
     mlapi_sup:start_link().
 
 stop(_State) ->
     ok.
+
+
+-spec ensure_mnesia_schema_created(node()) -> ok | {error, Reason :: term()}.
+ensure_mnesia_schema_created(Node) ->
+    %% This is a "hackish" way of making sure that a Mnesia schema exists on
+    %% disk without shutting down Mnesia.
+    %% When Mnesia is started without having called mnesia:create_schema/1 in
+    %% advance, Mnesia will start with its 'schema' table in RAM and any attempt
+    %% to create a table with a 'disc_copies' storage mode will fail. Also,
+    %% mnesia:create_schema/1 will fail if called while Mnesia is up.
+    %% To solve this problem, we use mnesia:change_table_copy_type/3 to make
+    %% the default 'schema' disk-based.
+    case mnesia:change_table_copy_type(schema, Node, disc_copies) of
+        {atomic, ok} ->
+            error_logger:info_msg("Created Mnesia schema for '~s'~n", [Node]),
+            ok;
+        {aborted, {already_exists, schema, Node, disc_copies}} ->
+            ok;
+        Error ->
+            Error
+    end.
